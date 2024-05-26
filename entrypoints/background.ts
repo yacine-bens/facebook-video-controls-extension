6ebb847dc0f0ebcd4ff690a7d859ea6ffbe97bc6
yacine-bens <82149396+yacine-bens@users.noreply.github.com>
2024-05-27 00:57:09 +0100

    Reel in a Facebook post Fixes #6

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,23 +1,31 @@
 const showVideoControls = () => {
   // Reels
   if (window.location.href.includes('reel')) {
-    const videoElement = document.evaluate('//div[@data-video-id]//video', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue as HTMLVideoElement;
-    if (!videoElement) {
+    let xpathResult = document.evaluate('//div[@data-video-id]//video', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    const videoElements = Array.from({ length: xpathResult.snapshotLength }, (_, i) => xpathResult.snapshotItem(i)) as HTMLVideoElement[];
+
+    if (!videoElements.length) {
       console.log('No video element found');
       return;
     }
-    videoElement.controls = true;
-    const nextSibling = videoElement.nextSibling as HTMLDivElement;
-    if (nextSibling) {
-      nextSibling.style.display = 'none';
-    }
-
-    const hiddenDiv = document.evaluate('//div[preceding-sibling::div[div[@data-video-id]]]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue as HTMLDivElement;
-    if (!hiddenDiv) {
-      console.log('No hidden div found');
-      return;
-    }
-    hiddenDiv.style.display = 'none';
+    videoElements.forEach(video =>{ 
+      video.controls = true
+      
+      const nextSibling = video.nextSibling as HTMLDivElement;
+      if (nextSibling) {
+        nextSibling.style.display = 'none';
+      }
+  
+      xpathResult = document.evaluate('//div[preceding-sibling::div[div[@data-video-id]]]', document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      const hiddenDivElements = Array.from({ length: xpathResult.snapshotLength }, (_, i) => xpathResult.snapshotItem(i)) as HTMLDivElement[];
+      if (!hiddenDivElements.length) {
+        console.log('No hidden div found');
+        return;
+      }
+      hiddenDivElements.forEach(hiddenDiv => {
+        hiddenDiv.style.display = 'none';
+      });
+    });
   }
   // Stories
   else if (window.location.href.includes('stories')) {
