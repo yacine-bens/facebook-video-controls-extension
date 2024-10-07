8a94feaf2c77cd4bb2e1580241c4e39f96325fa0
dth <dth@unistats.pro>
2024-10-07 15:39:53 -0500

    Fix: volume level between reels

--- assets/showVideoControls.ts
+++ assets/showVideoControls.ts
@@ -1,24 +1,37 @@
-const showVideoControls = () => {
-	const { href } = window.location;
-	if (!href.includes('reel') && !href.includes('stories')) return;
+const showVideoControls = async () => {
+  const { href } = window.location;
+  if (!href.includes('reel') && !href.includes('stories')) return;
 
-	const videoElements = document.querySelectorAll('video');
-	if (!videoElements.length) return;
+  const videoElements = document.querySelectorAll('video');
+  if (!videoElements.length) return;
 
-	for (const videoElement of videoElements) {
-		videoElement.controls = true;
-		const nextSibling = videoElement.nextSibling as HTMLDivElement;
-		if (nextSibling && nextSibling.getAttributeNames().includes('data-instancekey')) {
-			nextSibling.style.display = 'none';
-		}
+  for (const videoElement of videoElements) {
+    videoElement.controls = true;
+    
+    const volumeActual = await new Promise(resolve => 
+      chrome.runtime.sendMessage({type: 'GET_VOLUME'}, resolve)
+    );
+    
+    videoElement.volume = volumeActual as number || 0.1;
 
-		if (href.includes('reel')) {
-			const hiddenDiv = document.evaluate('.//ancestor::div[@data-video-id]/parent::div/following-sibling::div', videoElement, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue as HTMLDivElement;
-			if (hiddenDiv) {
-				hiddenDiv.style.display = 'none';
-			}
-		}
-	}
+    videoElement.addEventListener('volumechange', (event) => {
+      const video = event.target as HTMLVideoElement;
+      const newVolume = video.volume;
+      chrome.runtime.sendMessage({type: 'SET_VOLUME', volume: newVolume});
+    });
+
+    const nextSibling = videoElement.nextSibling as HTMLDivElement;
+    if (nextSibling && nextSibling.getAttributeNames().includes('data-instancekey')) {
+      nextSibling.style.display = 'none';
+    }
+
+    if (href.includes('reel')) {
+      const hiddenDiv = document.evaluate('.//ancestor::div[@data-video-id]/parent::div/following-sibling::div', videoElement, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue as HTMLDivElement;
+      if (hiddenDiv) {
+        hiddenDiv.style.display = 'none';
+      }
+    }
+  }
 };
 
 export default showVideoControls;
